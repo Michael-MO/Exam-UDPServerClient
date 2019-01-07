62000cee2077aa3f8067e5edc9453c963b66a7db
Michael-MO <Michael-MO@users.noreply.github.com>
2019-01-07 21:14:59 +0100

    Init Commit

---
+++ Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Server
+{
+    public class Program
+    {
+        private const int PORT = 7070;
+
+        public static void Main(string[] args)
+        {
+            Server server = new Server(PORT);
+            server.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class Server
+    {
+        private static List<string> namesList = new List<string>();
+        private int PORT;
+
+        public Server(int PORT)
+        {
+            this.PORT = PORT;
+        }
+
+        public void Start()
+        {
+            IPEndPoint removeEP = new IPEndPoint(IPAddress.Any, PORT);
+
+            using (UdpClient receiverSocket = new UdpClient(PORT))
+            {
+                while (true)
+                {
+                    HandleOneRequest(receiverSocket, removeEP);
+                }
+            }
+        }
+
+        private void HandleOneRequest(UdpClient receiverSocket, IPEndPoint removeEP)
+        {
+            byte[] data = receiverSocket.Receive(ref removeEP);
+            string inString = Encoding.ASCII.GetString(data);
+
+            if (!namesList.Contains(inString))
+            {
+                namesList.Add(inString);
+
+                Console.WriteLine("Modtaget : " + inString);
+                Console.WriteLine("Sender IP : " + removeEP.Address + ", PORT : " + removeEP.Port);
+            }
+            else
+            {
+            }
+
+
+            byte[] outData = Encoding.ASCII.GetBytes(inString.ToUpper());
+            receiverSocket.Send(outData, outData.Length, removeEP);
+        }
+    }
+}
