1173dec5aa1c9d406289d06709e66169a8c48a7f
Michael-MO <Michael-MO@users.noreply.github.com>
2019-01-07 21:30:10 +0100

    Add Client Project

---
+++ Client/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class Client
+    {
+        private int PORT;
+
+        public Client(int PORT)
+        {
+            this.PORT = PORT;
+        }
+
+        public void Start()
+        {
+            // Car obj = new Car("Tesla", "Red", "EL23400");
+            string message = "Michael";
+
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            IPEndPoint receiverEP = new IPEndPoint(IPAddress.Broadcast, PORT);
+
+            using (UdpClient senderSocket = new UdpClient()) // Ingen Port
+            {
+                senderSocket.EnableBroadcast = true;
+                senderSocket.Send(data, data.Length, receiverEP);
+
+                IPEndPoint receiveEP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] inData = senderSocket.Receive(ref receiverEP);
+
+                string inString = Encoding.ASCII.GetString(inData);
+
+                //Console.WriteLine("Modtaget : " + inString);
+            }
+        }
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Client
+{
+    public class Program
+    {
+        private const int PORT = 7070;
+        public static void Main(string[] args)
+        {
+            Client client = new Client(PORT);
+            client.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
